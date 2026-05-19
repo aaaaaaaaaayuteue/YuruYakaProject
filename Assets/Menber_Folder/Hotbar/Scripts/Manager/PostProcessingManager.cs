054bf7a9a9e6c88f15f56509c22a065df268d17f
Unsama <kws4510@naver.com>
2026-05-19 19:11:50 +0900

    [Test] PostProcessing

--- Assets/Menber_Folder/Hotbar/Scripts/Manager/PostProcessingManager.cs
+++ Assets/Menber_Folder/Hotbar/Scripts/Manager/PostProcessingManager.cs
@@ -1,17 +1,42 @@
 using Hotbar.Base;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Hotbar.Manager
 {
     public class PostProcessingManager : SingletonMonobase<PostProcessingManager>
     {
+        [System.Serializable]
+        public class PostProcessData
+        {
+            [Header("Volume")]
+            [SerializeField] private Volume volume;
+
+            [Header("Override Option")]
+            private Bloom bloom;
+            private WhiteBalance whiteBalance;
+            private ColorAdjustments colorAdjustments;
+
+            public void Initialize()
+            {
+                volume.profile.TryGet(out colorAdjustments);
+                volume.profile.TryGet(out whiteBalance);
+                volume.profile.TryGet(out bloom);
+            }
+
+            public Volume GetVolume() => volume;
+            public Bloom GetBloom() => bloom;
+            public WhiteBalance GetWhiteBalance() => whiteBalance;
+            public ColorAdjustments GetColorAdjustments() => colorAdjustments;
+        }
+
         [Header("Postprocessing Volume")]
-        [SerializeField] private Volume volume;
+        [SerializeField] private PostProcessData postProcessData;
 
         public static void Initialize()
         {
-
+            Instance.postProcessData.Initialize();
         }
     }
 }
